c117b9ba483473d9b4dcc121009e3a6511ab1447
Shark43 <talia4343@gmail.com>
2021-10-08 11:06:20 +0200

    init

---
+++ cmake-build-debug/CMakeFiles/LAB02ES02.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/LAB02ES02.dir/main.c.obj"
+  "LAB02ES02.exe"
+  "LAB02ES02.exe.manifest"
+  "LAB02ES02.pdb"
+  "libLAB02ES02.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/LAB02ES02.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
